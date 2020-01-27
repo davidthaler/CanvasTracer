37be94615a7efe409b7c49b1ba765b2105e2c032
David Thaler <davidthaler@gmail.com>
2020-01-26 20:40:43 -0800

    Added undo functionality with a stack of curves.

--- static/mobileCanvas.js
+++ static/mobileCanvas.js
@@ -1,7 +1,20 @@
 let canvas, context, isDrawing
+let data = []
 
 function shiftXY(e){
     return [e.pageX - canvas.offsetLeft, e.pageY - canvas.offsetTop]
+}
+
+function startCurve(x, y){
+    data.push({
+        lineWidth: context.lineWidth,
+        color: context.strokeStyle,
+        points: [[x, y]]
+    })
+}
+
+function addPoint(x, y){
+    data[data.length - 1].points.push([x, y])
 }
 
 function start(e){
@@ -9,6 +22,7 @@ function start(e){
     let [x, y] = shiftXY(e.touches[0])
     context.beginPath()
     context.moveTo(x, y)
+    startCurve(x, y)
 }
 
 function draw(e){
@@ -16,6 +30,7 @@ function draw(e){
     let [x, y] = shiftXY(e.touches[0])
     context.lineTo(x, y)
     context.stroke()
+    addPoint(x, y)
 }
 
 function drawMode(){
@@ -35,8 +50,32 @@ function drawMode(){
     postTime()
 }
 
+function redraw(){
+    for(let path of data){
+        context.lineWidth = path.lineWidth
+        context.strokeStyle = path.color
+        context.beginPath()
+        context.moveTo(...path.points[0])
+        for(let point of path.points.slice(1)){
+            context.lineTo(...point)
+        }
+        context.stroke()
+    }
+}
+
 function clearCanvas(){
     context.clearRect(0, 0, canvas.width, canvas.height)
+}
+
+function undo(){
+    data.pop()
+    clearCanvas()
+    redraw()
+}
+
+function reset(){
+    data = []
+    clearCanvas()
 }
 
 function postTime(){
@@ -56,7 +95,8 @@ window.addEventListener('load', function(){
     canvas.height = canvas.parentElement.clientHeight
     context = canvas.getContext('2d')
     document.getElementById('drawBtn').addEventListener('click', drawMode)
-    document.getElementById('clearBtn').addEventListener('click', clearCanvas)
+    document.getElementById('clearBtn').addEventListener('click', reset)
+    document.getElementById('undoBtn').addEventListener('click', undo)
     document.getElementById('picker')
             .addEventListener('change', function(){
                 context.strokeStyle = this.value
